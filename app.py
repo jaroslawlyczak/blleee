bd4ac58fb003594e76f5106c8acd01ff9904b45a
jaroslawlyczak <116738242+jaroslawlyczak@users.noreply.github.com>
2025-01-09 14:51:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
-
-import pytz
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hell_name(name):
-    return 'Hello %s!' % name
+tasks = []
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        new_task = request.form['task']
+        tasks.append(new_task)
+        return redirect(url_for('index'))
+
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:task_index>')
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        tasks.pop(task_index)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
